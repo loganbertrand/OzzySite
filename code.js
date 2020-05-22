6de7e73d47ab398d425ac3e016eef436777aa3ea
LoganB <loganwbertrand@gmail.com>
2020-05-22 12:06:06 -0700

    sidenav working as intended

--- code.js
+++ code.js
@@ -15,3 +15,16 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "350px";
+ 
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+ 
+}
